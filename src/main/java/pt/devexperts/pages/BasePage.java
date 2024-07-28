d9343b6e84cc9a226eb42e78c623f66e4eaced6d
nikit34 <permikov134@mail.ru>
2024-07-28 02:10:07 +0100

    Add pages

---
+++ src/main/java/pt/devexperts/pages/BasePage.java
@@ -0,0 +1,8 @@
+package pt.devexperts.pages;
+
+
+
+public class BasePage {
+    public static final MainPage mainPage = new MainPage();
+    public static final CartPage cartPage = new CartPage();
+}
---
+++ src/main/java/pt/devexperts/pages/MainPage.java
@@ -0,0 +1,4 @@
+package pt.devexperts.pages;
+
+public class MainPage {
+}
